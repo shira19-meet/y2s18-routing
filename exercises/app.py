32570fd68a12f5dd0f4771f5d33ff4bdd0bd56bb
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 14:12:23 +0300

    djhhd

--- exercises/app.py
+++ exercises/app.py
@@ -2,8 +2,27 @@ from databases import *
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def home():
-    return 'START HERE: CHANGE ME'
+    return render_template(
+    	'home.html')
+
+@app.route('/<int:student_id>')
+def display_student(student_id):
+	return render_template(
+    	'student.html' ,s=student_id)
+@app.route('/<int:student_name>')
+def display_student(student_name):
+	return render_template(
+    	'student.html' ,n=student_name)
+	@app.route('/<int:student_yaer>')
+def display_student(student_year):
+	return render_template(
+    	'student.html' ,y=student_year)
+
+
+
 
 app.run(debug=True)
